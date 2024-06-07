ccfbede88885b443cadad002826385d33371797f
kakeru-ikeda <kakerumail1451@gmail.com>
2024-06-07 21:59:38 +0900

    add: WarehouseDetailViewにPlaceholder追加

---
+++ lib/View/Component/warehouse_detail_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WarehouseDetailView extends StatefulWidget {
+  const WarehouseDetailView({super.key});
+
+  @override
+  State<WarehouseDetailView> createState() => _WarehouseDetailViewState();
+}
+
+class _WarehouseDetailViewState extends State<WarehouseDetailView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
