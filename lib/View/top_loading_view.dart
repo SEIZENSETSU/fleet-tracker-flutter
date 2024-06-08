dbba65da331b1107520e3541d427d4313231ee35
kakeru-ikeda <kakerumail1451@gmail.com>
2024-06-08 16:42:26 +0900

    add: TopLoadingの追加

---
+++ lib/View/top_loading_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TopLoadingView extends StatelessWidget {
+  const TopLoadingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
