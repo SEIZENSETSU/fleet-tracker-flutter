0a5971afa9375677df1e54f2c4221ac01358795e
kakeru-ikeda <kakerumail1451@gmail.com>
2024-06-09 21:17:53 +0900

    add: TopLoadingViewの仮実装

--- lib/View/top_loading_view.dart
+++ lib/View/top_loading_view.dart
@@ -1,4 +1,6 @@
 import 'package:fleet_tracker/Controller/top_loading_controller.dart';
+import 'package:fleet_tracker/gen/assets.gen.dart';
+import 'package:fleet_tracker/gen/colors.gen.dart';
 import 'package:flutter/material.dart';
 
 class TopLoadingView extends StatelessWidget {
@@ -8,8 +10,53 @@ class TopLoadingView extends StatelessWidget {
   Widget build(BuildContext context) {
     TopLoadingController(context).firstLoad();
 
-    return const Scaffold(
-      body: Center(child: Text('TopLoadingView')),
+    final double deviceHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: deviceHeight,
+            child: Assets.images.topLoading.background.image(
+              fit: BoxFit.cover,
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: Assets.images.topLoading.main.image(),
+                ),
+                SizedBox(height: deviceHeight * 0.1),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 18,
+                      width: 18,
+                      child: CircularProgressIndicator(
+                        valueColor:
+                            AlwaysStoppedAnimation<Color>(ColorName.splashMain),
+                        strokeWidth: 3.0,
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Text(
+                      '起動中...',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
