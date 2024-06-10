e1e2ff4898895aa1744b990327d522b3c9b2da81
ishibashikeita <109339217+ishibashikeita@users.noreply.github.com>
2024-06-10 17:00:27 +0900

    add: initコミット　ブランチ分け

--- lib/View/Home/home_view.dart
+++ lib/View/Home/home_view.dart
@@ -10,6 +10,15 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'こんにちは',
+          style: TextStyle(
+            fontSize: 40,
+          ),
+        ),
+      ),
+    );
   }
 }
