d5efb193974284dd9f880847f42b829282d57b6d
ryo hasegawa <minecraftnolisio@gmail.com>
2024-06-13 16:04:01 +0900

    add: warehouse model 追加

---
+++ lib/Model/Entity/warehouse.dart
@@ -0,0 +1,14 @@
+class Warehouse {
+  Warehouse({
+    required this.warehouse_id,
+    required this.warehouse_area_id,
+    required this.warehouse_name,
+    required this.warehouse_latitude,
+    required this.warehouse_longitude,
+  });
+  final int warehouse_id;
+  final int warehouse_area_id;
+  final String warehouse_name;
+  final double warehouse_latitude;
+  final double warehouse_longitude;
+}
