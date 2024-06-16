f2303c9f8e1daad52f281892eb152a4e3fcceb11
ishibashikeita <109339217+ishibashikeita@users.noreply.github.com>
2024-06-16 22:55:58 +0900

    fix: customScaffold 修正

--- lib/View/Component/CustomScaffold/custom_scaffold.dart
+++ lib/View/Component/CustomScaffold/custom_scaffold.dart
@@ -1,43 +1,29 @@
-import 'package:fleet_tracker/View/Component/CustomWidget/custom_appbar.dart';
 import 'package:fleet_tracker/gen/colors.gen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class CustomScaffold extends StatelessWidget {
   CustomScaffold({
     super.key,
-    required this.contentList,
+    this.appBar,
+    required this.body,
+    this.floatingActionButton,
+    this.backgroundColor = ColorName.scaffoldBackground,
   });
 
-  List<Widget> contentList;
+  PreferredSizeWidget? appBar;
+  Widget body;
+  FloatingActionButton? floatingActionButton;
+  Color? backgroundColor;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorName.mainthemeColor,
-      appBar: CustomAppBar(),
-      body: Stack(
-        children: [
-          Container(
-            margin:
-                const EdgeInsets.only(left: 20, right: 20, bottom: 10, top: 10),
-            height: double.infinity,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  children: contentList,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+      backgroundColor: backgroundColor,
+      floatingActionButton: floatingActionButton,
+      appBar: appBar,
+      body: body,
     );
   }
 }
