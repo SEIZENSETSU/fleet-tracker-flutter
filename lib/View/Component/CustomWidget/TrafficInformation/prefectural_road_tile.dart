b0db115d42270ddf3ba47643d6066ef2cf8b70df
school\rei20 <nakada0404rei@gmail.com>
2024-06-19 20:51:52 +0900

    add: 消えた学校作業分

---
+++ lib/View/Component/CustomWidget/TrafficInformation/prefectural_road_tile.dart
@@ -0,0 +1,83 @@
+import 'package:fleet_tracker/View/Component/CustomWidget/custom_text.dart';
+import 'package:fleet_tracker/gen/colors.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class PrefecturalRoadTile extends StatelessWidget {
+  const PrefecturalRoadTile({super.key, required this.prefecturalRoadName});
+
+  final String prefecturalRoadName;
+
+  @override
+  Widget build(BuildContext context) {
+    double _deviceWidth = MediaQuery.of(context).size.width;
+    return GestureDetector(
+      onTap: () {
+        print(prefecturalRoadName);
+      },
+      child: Container(
+        width: _deviceWidth,
+        height: _deviceWidth * 0.1,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: _deviceWidth * 0.2,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: BorderDirectional(
+                  bottom: BorderSide(
+                    width: 1,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              width: _deviceWidth * 0.8,
+              height: _deviceWidth * 0.1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    width: _deviceWidth * 0.6,
+                    height: _deviceWidth * 0.06,
+                    child: FittedBox(
+                      alignment: Alignment.centerLeft,
+                      fit: BoxFit.fitHeight,
+                      child: CustomText(
+                        text: prefecturalRoadName,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: _deviceWidth * 0.1,
+                    height: _deviceWidth * 0.06,
+                    child: FittedBox(
+                      alignment: Alignment.centerRight,
+                      fit: BoxFit.fitHeight,
+                      child: Icon(
+                        Icons.warning,
+                        color: Colors.amber,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: _deviceWidth * 0.1,
+                    height: _deviceWidth * 0.05,
+                    child: FittedBox(
+                      fit: BoxFit.fitHeight,
+                      child: Icon(
+                        Icons.arrow_forward_ios,
+                        color: ColorName.mainthemeColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/View/Component/CustomWidget/TrafficInformation/traffic_information_tile_cell.dart
@@ -0,0 +1,122 @@
+import 'package:fleet_tracker/View/Component/CustomWidget/TrafficInformation/prefectural_road_tile.dart';
+import 'package:fleet_tracker/View/Component/CustomWidget/custom_text.dart';
+import 'package:fleet_tracker/gen/colors.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TrafficInformationTileCell extends StatelessWidget {
+  const TrafficInformationTileCell({
+    super.key,
+    required this.imageUrl,
+    required this.count,
+    required this.prefectureName,
+    required this.prefecturalRoadList,
+    required this.index,
+  });
+
+  final String imageUrl;
+  final int count;
+  final String prefectureName;
+  final List<String> prefecturalRoadList;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    double _deviceWidth = MediaQuery.of(context).size.width;
+    List<bool> tapCheckList = [
+      true,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false
+    ];
+
+    return Column(
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            print(prefecturalRoadList);
+            print(index);
+            tapCheckList[index] != tapCheckList[index];
+            print(tapCheckList[index]);
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              border: BorderDirectional(
+                bottom: BorderSide(
+                  width: 1,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            width: _deviceWidth,
+            child: Row(
+              children: <Widget>[
+                SizedBox(
+                  width: _deviceWidth * 0.2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Image.network(imageUrl),
+                  ),
+                ),
+                SizedBox(
+                  width: _deviceWidth * 0.3,
+                  height: _deviceWidth * 0.06,
+                  child: FittedBox(
+                      alignment: Alignment.centerLeft,
+                      fit: BoxFit.fitHeight,
+                      child: CustomText(
+                        text: prefectureName,
+                      )),
+                ),
+                SizedBox(
+                  width: _deviceWidth * 0.2,
+                  height: _deviceWidth * 0.06,
+                  child: FittedBox(
+                    alignment: Alignment.centerLeft,
+                    fit: BoxFit.fitHeight,
+                    child: CustomText(text: '${count}件'),
+                  ),
+                ),
+                SizedBox(
+                  width: _deviceWidth * 0.3,
+                  height: _deviceWidth * 0.08,
+                  child: const FittedBox(
+                    alignment: Alignment.centerRight,
+                    fit: BoxFit.fitHeight,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        right: 8,
+                      ),
+                      child: Icon(
+                        Icons.expand_more,
+                        color: ColorName.mainthemeColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Visibility(
+          visible: tapCheckList[index],
+          child: Column(
+            children: <Widget>[
+              for (int i = 0; i < prefecturalRoadList.length; i++) ...{
+                PrefecturalRoadTile(
+                    prefecturalRoadName: prefecturalRoadList[i]),
+              }
+            ],
+          ),
+        ),
+      ],
+    );
+
+    ;
+  }
+}
