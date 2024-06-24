03b4bdb357db63af363ebc0f80c779c6f84d54b6
kakeru-ikeda <kakerumail1451@gmail.com>
2024-06-25 02:15:22 +0900

    fix: アプリのライフサイクルを検知するために、ボトムタブをStatefulWidgetに変更

--- lib/View/bottom_navigation_bar_view.dart
+++ lib/View/bottom_navigation_bar_view.dart
@@ -2,18 +2,42 @@ import 'package:flutter/material.dart';
 
 import '../Controller/bottom_navigation_bar_controller.dart';
 
-class BottomNavigationBarView extends StatelessWidget {
+class BottomNavigationBarView extends StatefulWidget {
   const BottomNavigationBarView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final BottomNavigationBarController controller =
-        BottomNavigationBarController();
+  State<BottomNavigationBarView> createState() =>
+      _BottomNavigationBarViewState();
+}
 
+class _BottomNavigationBarViewState extends State<BottomNavigationBarView>
+    with WidgetsBindingObserver {
+  late BottomNavigationBarController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = BottomNavigationBarController();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    _controller.appLifecycleState(state);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: controller.navigationShell,
+      body: _controller.navigationShell,
       bottomNavigationBar: NavigationBar(
-        selectedIndex: controller.navigationShell.currentIndex,
+        selectedIndex: _controller.navigationShell.currentIndex,
         destinations: const [
           NavigationDestination(icon: Icon(Icons.home), label: 'ホーム'),
           NavigationDestination(icon: Icon(Icons.search), label: '倉庫検索'),
@@ -22,16 +46,16 @@ class BottomNavigationBarView extends StatelessWidget {
           NavigationDestination(icon: Icon(Icons.settings), label: '設定'),
         ],
         onDestinationSelected: (index) {
-          controller.goBranch(index);
+          _controller.goBranch(index);
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          controller.goBranch(2);
+          _controller.goBranch(2);
         },
         foregroundColor: Colors.white,
-        backgroundColor: controller.getFloatingActionButtonColor(2),
+        backgroundColor: _controller.getFloatingActionButtonColor(2),
         child: const Icon(Icons.add),
       ),
     );
