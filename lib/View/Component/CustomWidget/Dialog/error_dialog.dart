7c197a57b64f0112520ea9417588f2992e6de25e
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-06-30 20:50:10 +0900

    add: error_dialog_icon画像の追加とErrorDialogのデザイン調整

--- lib/View/Component/CustomWidget/Dialog/error_dialog.dart
+++ lib/View/Component/CustomWidget/Dialog/error_dialog.dart
@@ -1,10 +1,7 @@
-import 'package:fleet_tracker/Constants/strings.dart';
 import 'package:fleet_tracker/View/Component/CustomWidget/custom_button.dart';
 import 'package:fleet_tracker/View/Component/CustomWidget/custom_text.dart';
 import 'package:fleet_tracker/gen/colors.gen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 
 class ErrorDialog {
@@ -13,13 +10,14 @@ class ErrorDialog {
     required String title,
     required Widget content,
     required String detail,
+    required String buttonText,
     Function? buttonAction,
     bool barrierDismissible = false,
   }) {
     showDialog(
       context: context,
-      barrierColor: Colors.transparent,
-      // barrierDismissible: barrierDismissible,
+      barrierColor: Colors.black.withOpacity(0.4),
+      barrierDismissible: barrierDismissible,
       builder: (_) {
         return Dialog(
           backgroundColor: Colors.transparent,
@@ -29,20 +27,11 @@ class ErrorDialog {
           ),
           child: Container(
             width: 150,
-            // height: 200,
             decoration: BoxDecoration(
               border: Border.all(
                 color: Colors.black,
               ),
               color: Colors.white,
-              boxShadow: const [
-                BoxShadow(
-                  color: ColorName.mainthemeColor,
-                  spreadRadius: 1.0,
-                  blurRadius: 0,
-                  offset: Offset(5, 5),
-                )
-              ],
             ),
             child: Column(
               mainAxisSize: MainAxisSize.min,
@@ -50,18 +39,19 @@ class ErrorDialog {
               children: [
                 Container(
                   height: 50,
-                  color: ColorName.mainthemeColor.withOpacity(0.1),
+                  color: ColorName.mainthemeColor,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Container(
-                        padding: EdgeInsets.only(left: 10),
+                        padding: const EdgeInsets.only(left: 10),
                         height: 30,
                         width: 200,
                         child: FittedBox(
                           fit: BoxFit.contain,
                           child: CustomText(
                             text: title,
+                            color: Colors.white,
                           ),
                         ),
                       ),
@@ -71,57 +61,62 @@ class ErrorDialog {
                 Container(
                   height: 180,
                   color: Colors.white,
-                  margin: EdgeInsets.all(10),
-                  child: Container(
-                    child: Column(
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Container(
-                            child: Center(
-                              child: content,
+                  margin: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  right: 10,
+                                ),
+                                child: Center(
+                                  child: content,
+                                ),
+                              ),
                             ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            child: FittedBox(
-                              fit: BoxFit.fitWidth,
+                            Expanded(
+                              flex: 3,
                               child: Row(
                                 children: [
-                                  CustomText(
-                                    text: detail,
+                                  Flexible(
+                                    child: CustomText(
+                                      text: detail,
+                                    ),
                                   ),
                                 ],
                               ),
                             ),
-                          ),
+                          ],
                         ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            width: 200,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 5),
-                              child: CustomButton(
-                                primaryColor: ColorName.mainthemeColor,
-                                text: Strings.BACK_BUTTON_TEXT,
-                                onTap: buttonAction != null
-                                    ? () {
-                                        // 指定した処理
-                                        buttonAction();
-                                      }
-                                    : () {
-                                        // 戻る
-                                        context.pop();
-                                      },
-                              ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: SizedBox(
+                          width: 200,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: CustomButton(
+                              primaryColor: ColorName.mainthemeColor,
+                              text: buttonText,
+                              onTap: buttonAction != null
+                                  ? () {
+                                      // 指定した処理
+                                      buttonAction();
+                                    }
+                                  : () {
+                                      // 戻る
+                                      context.pop();
+                                    },
                             ),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ],
