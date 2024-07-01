ab1a65128a8b994aeedbdbdea86dd4d1138e4b9a
ryo hasegawa <minecraftnolisio@gmail.com>
2024-07-01 15:28:00 +0900

    fix: modelの

--- lib/Model/Entity/Weather/weather.dart
+++ lib/Model/Entity/Weather/weather.dart
@@ -10,20 +10,20 @@ class Weather {
   });
 
   factory Weather.nowDataFromJson(Map<String, dynamic> json) {
-    const int minuets = 0;
+    const int minutes = 0;
     return Weather(
-      type: json['YDF']['Feature']['Property']['WeatherList']['Weather'][minuets]['Type'], // json['type
-      date: json['YDF']['Feature']['Property']['WeatherList']['Weather'][minuets]['Date'], // json['date']
-      rainfall: double.parse(json['YDF']['Feature']['Property']['WeatherList']['Weather'][minuets]['Rainfall']),
+      type: json['YDF']['Feature']['Property']['WeatherList']['Weather'][minutes]['Type'], // json['type
+      date: json['YDF']['Feature']['Property']['WeatherList']['Weather'][minutes]['Date'], // json['date']
+      rainfall: double.parse(json['YDF']['Feature']['Property']['WeatherList']['Weather'][minutes]['Rainfall']),
     );
   }
 
   factory Weather.afterOneHourDataFromJson(Map<String, dynamic> json) {
-    const int minuets = 6;
+    const int minutes = 6;
     return Weather(
-      type: json['YDF']['Feature']['Property']['WeatherList']['Weather'][minuets]['Type'], // json['type
-      date: json['YDF']['Feature']['Property']['WeatherList']['Weather'][minuets]['Date'], // json['date']
-      rainfall: double.parse(json['YDF']['Feature']['Property']['WeatherList']['Weather'][minuets]['Rainfall']),
+      type: json['YDF']['Feature']['Property']['WeatherList']['Weather'][minutes]['Type'], // json['type
+      date: json['YDF']['Feature']['Property']['WeatherList']['Weather'][minutes]['Date'], // json['date']
+      rainfall: double.parse(json['YDF']['Feature']['Property']['WeatherList']['Weather'][minutes]['Rainfall']),
     );
   }
 }
