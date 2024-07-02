bdca6b5c4d65d5e4b1b32f5a16e4eb3469482a15
ryo hasegawa <minecraftnolisio@gmail.com>
2024-07-02 18:04:52 +0900

    add:道路名取得するservice追加

--- lib/Service/API/Original/road_information_service.dart
+++ lib/Service/API/Original/road_information_service.dart
@@ -1,80 +1,68 @@
 import 'dart:convert';
-import 'dart:math';
-
+import 'package:fleet_tracker/Service/Log/log_service.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import 'package:fleet_tracker/Constants/Enum/road_types_enum.dart';
-import 'package:fleet_tracker/Model/Entity/road.dart';
 
 class RoadInformationService {
-  double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-    const double R = 6371; // 地球の半径 (km)
+  late String baseUrl;
 
-    double dLat = (lat2 - lat1) * pi / 180;
-    double dLon = (lon2 - lon1) * pi / 180;
-    double a = sin(dLat / 2) * sin(dLat / 2) + cos(lat1 * pi / 180) * cos(lat2 * pi / 180) * sin(dLon / 2) * sin(dLon / 2);
-    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-
-    // 距離をメートルで返す
-    return R * c * 1000;
+  RoadInformationService() {
+    baseUrl = dotenv.env['ROAD_INFORMATION_API_BASEURL']!;
   }
 
-  Future<Road?> getNearestRoad(double lat, double lon) async {
-    const double radius = 8; // 半径（メートル）
-    const String overpassUrl = 'http://overpass-api.de/api/interpreter';
+  /// 最寄りの高速道路名取得
+  /// [latitude] 緯度
+  /// [longitude] 経度
+  Future<String?> getNearestRoadName({
+    required double latitude,
+    required double longitude,
+  }) async {
+    /// 検索範囲
+    const int radius = 100;
 
-    String overpassQuery = '''
-    [out:json];
-    (
-      way["highway"]
-        (around:$radius,$lat,$lon);
-    );
-    out body;
-    >;
-    out skel;
-  ''';
+    /// 検索条件のクエリ
+    final String query = '''
+      [out:json];
+      (
+        way["highway" = "motorway"]
+          (around:$radius,$latitude,$longitude);
+      );
+      out body;
+      >;
+      out skel;
+    ''';
 
-    final response = await http.get(
-      Uri.parse(overpassUrl),
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-    );
+    try {
+      Uri uri = Uri.https(
+        baseUrl,
+        '/api/interpreter',
+        {'data': query},
+      );
 
-    if (response.statusCode != 200) {
-      throw Exception('Failed to load data');
-    }
+      http.Response response = await http.get(uri);
 
-    final data = json.decode(response.body);
-    final elements = data['elements'];
-    final nodes = elements.where((element) => element['type'] == 'node').toList();
-    final Map<int, dynamic> nodeMap = {};
-
-    for (var node in nodes) {
-      nodeMap[node['id']] = node;
-    }
-
-    Road? nearestRoad;
-    double minDistance = double.infinity;
-
-    final roads = elements.where((element) => element['type'] == 'way').toList();
-
-    for (var road in roads) {
-      for (var nodeId in road['nodes']) {
-        final node = nodeMap[nodeId];
-        if (node != null) {
-          final double nodeLat = node['lat'];
-          final double nodeLon = node['lon'];
-          final double distance = calculateDistance(lat, lon, nodeLat, nodeLon);
-          if (distance < minDistance) {
-            minDistance = distance;
-            nearestRoad = Road(
-              highway: road['tags']['highway'],
-              type: RoadTypeEnumType(road['tags']['highway']),
-              name: road['tags']['name'],
-            );
-          }
-        }
+      if (response.statusCode != 200) {
+        throw Exception('Fetch failed.');
       }
-    }
 
-    return nearestRoad;
+      Map<String, dynamic> jsonResponse = json.decode(
+        utf8.decode(response.bodyBytes),
+      );
+
+      /// 付近100m以内に高速道路が見つからなかった場合、
+      /// 走っていないと判定してnullを返す
+      if (jsonResponse['elements'].isEmpty) {
+        Log.echo('今は高速道路を走っていません');
+        return null;
+      }
+
+      String nearestRoadName = jsonResponse['elements'][0]['tags']['name'];
+      Log.echo('取得成功');
+      Log.echo(nearestRoadName);
+      return nearestRoadName;
+    } catch (e) {
+      Log.echo('エラーが発生しました $e');
+      return null;
+    }
   }
 }
