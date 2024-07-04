30fcc60887360b95804fae8b5a5a84788d7bbf89
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-07-04 23:56:01 +0900

    fix: FloatingActionButtonの削除とbottomNavigationBarのデザイン修正

--- lib/View/bottom_navigation_bar_view.dart
+++ lib/View/bottom_navigation_bar_view.dart
@@ -1,6 +1,10 @@
 import 'package:fleet_tracker/Model/Data/Warehouse/search_info_data.dart';
 import 'package:fleet_tracker/Service/Log/log_service.dart';
+import 'package:fleet_tracker/View/Component/CustomWidget/custom_text.dart';
+import 'package:fleet_tracker/gen/colors.gen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../Controller/bottom_navigation_bar_controller.dart';
@@ -37,37 +41,81 @@ class _BottomNavigationBarViewState extends State<BottomNavigationBarView>
     BottomNavigationBarController controller = BottomNavigationBarController();
     return Scaffold(
       body: controller.navigationShell,
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: controller.navigationShell.currentIndex,
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: 'ホーム'),
-          NavigationDestination(icon: Icon(Icons.search), label: '倉庫検索'),
-          SizedBox(width: 0),
-          NavigationDestination(icon: Icon(Icons.traffic), label: '交通情報'),
-          NavigationDestination(icon: Icon(Icons.settings), label: '設定'),
-        ],
-        onDestinationSelected: (index) {
-          controller.goBranch(index);
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Consumer(
-        builder: (context, ref, _) {
-          final warehouseSearchInfo =
-              ref.watch(warehouseSearchInfoDataProvider);
-
-          return FloatingActionButton(
-            onPressed: () {
-              if (!warehouseSearchInfo.getData().isInvading) {
-                return;
-              }
-              controller.goBranch(2);
+      bottomNavigationBar: Stack(
+        alignment: Alignment.center,
+        clipBehavior: Clip.none,
+        children: [
+          NavigationBar(
+            selectedIndex: controller.navigationShell.currentIndex,
+            indicatorColor: Colors.green.withOpacity(0.2),
+            destinations: const [
+              NavigationDestination(icon: Icon(Icons.home), label: 'ホーム'),
+              NavigationDestination(icon: Icon(Icons.search), label: '倉庫検索'),
+              SizedBox(width: 0),
+              NavigationDestination(icon: Icon(Icons.traffic), label: '交通情報'),
+              NavigationDestination(icon: Icon(Icons.settings), label: '設定'),
+            ],
+            onDestinationSelected: (index) {
+              controller.goBranch(index);
             },
-            foregroundColor: Colors.white,
-            backgroundColor: controller.getFloatingActionButtonColor(2),
-            child: const Icon(Icons.add),
-          );
-        },
+          ),
+          Positioned(
+            top: -30,
+            child: Consumer(builder: (context, ref, _) {
+              final warehouseSearchInfo =
+                  ref.watch(warehouseSearchInfoDataProvider);
+              return GestureDetector(
+                onTap: () {
+                  if (!warehouseSearchInfo.getData().isInvading) {
+                    return;
+                  }
+                  controller.goBranch(2);
+                },
+                child: Container(
+                  width: 70,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: ColorName.mainthemeColor,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        spreadRadius: 1,
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: const Column(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 2,
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Icon(
+                            Icons.add,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Text(
+                          '投稿',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }),
+          ),
+        ],
       ),
     );
   }
