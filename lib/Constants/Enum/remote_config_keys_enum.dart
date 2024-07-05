b6a605f85b06ae3bd62c9127c62edeeed8f6fede
kakeru-ikeda <kakerumail1451@gmail.com>
2024-07-06 01:34:52 +0900

    add: RemoteConfigでインターバルの設定を追加

--- lib/Constants/Enum/remote_config_keys_enum.dart
+++ lib/Constants/Enum/remote_config_keys_enum.dart
@@ -2,10 +2,12 @@ enum RemoteConfigKeys {
   release,
   enableTrafficInformation,
   debugNotification,
+  debugDurationInterval
 }
 
 /**
  * release: bool -> falseのときは起動を拒否
  * enableTrafficInformation: bool -> falseのときは交通情報タブ遷移後にエラー表示
  * debugNotification: bool -> trueのときはデバッグ通知を表示
+ * debugDurationInterval: bool -> デバッグ通知の表示間隔15秒か？
  */
--- lib/Service/Package/BackgroundLocator/background_locator_service.dart
+++ lib/Service/Package/BackgroundLocator/background_locator_service.dart
@@ -31,9 +31,14 @@ Future<void> backgroundHandler(background_task.Location data) async {
 
     DateTime now = DateTime.now();
     Location currentLocation = LocationData().getData();
+    RemoteConfigService remoteConfigService = RemoteConfigService();
 
     /// 15秒以内の位置情報は無視する
-    if (!now.isAfter(currentLocation.time.add(const Duration(seconds: 15)))) {
+    Duration diff =
+        remoteConfigService.getBool(RemoteConfigKeys.debugDurationInterval)
+            ? const Duration(seconds: 15)
+            : const Duration(minutes: 3);
+    if (!now.isAfter(currentLocation.time.add(diff))) {
       return;
     }
 
@@ -43,7 +48,6 @@ Future<void> backgroundHandler(background_task.Location data) async {
     );
     WidgetsFlutterBinding.ensureInitialized();
     SharedPreferencesService prefs = SharedPreferencesService();
-    RemoteConfigService remoteConfigService = RemoteConfigService();
 
     /// Locationをインスタンス化
     Location location = Location(
