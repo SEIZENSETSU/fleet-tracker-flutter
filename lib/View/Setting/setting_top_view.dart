0d56dae5826fa914f3176bad687e53d3e19b58ce
kakeru-ikeda <kakerumail1451@gmail.com>
2024-07-08 18:05:18 +0900

    add: 設定ページに端末通知設定を追記

--- lib/View/Setting/setting_top_view.dart
+++ lib/View/Setting/setting_top_view.dart
@@ -11,6 +11,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../../Route/router.dart';
 import '../Component/CustomWidget/Modal/rename_modal.dart';
@@ -37,167 +38,176 @@ class _SettingTopViewState extends State<SettingTopView> {
       body: FutureBuilder(
         future: controller.init(),
         builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-          return Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: CustomText(
-                    text: Strings.SETTING_ACOUNT_INFO,
-                    fontSize: 14,
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: CustomText(
+                      text: Strings.SETTING_ACOUNT_INFO,
+                      fontSize: 14,
+                    ),
                   ),
                 ),
-              ),
-              SettingTileCell().withDetail(
-                title: 'ユーザー名',
-                detail: UserData().getData().name ?? "",
-                onTap: () async {
-                  // コメント投稿をする際の表示名を変更できる項目
-                  Log.echo('名前変更');
-                  await controller.showReNameModal(
-                      context: context,
-                      size: size,
-                      setState: () {
-                        setState(() {});
-                      });
-                  setState(() {});
-                },
-              ),
-              FutureBuilder(
-                future: controller.getBackgroundLocatorStatus(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState != ConnectionState.done) {
-                    return const CircularProgressIndicator();
-                  }
-                  return SettingTileCell().withDetail(
-                    title: '位置情報取得',
-                    detail: snapshot.data == true ? 'ON' : 'OFF',
-                    onTap: () {
-                      ErrorDialog().showErrorDialog(
+                SettingTileCell().withDetail(
+                  title: 'ユーザー名',
+                  detail: UserData().getData().name ?? "",
+                  onTap: () async {
+                    // コメント投稿をする際の表示名を変更できる項目
+                    Log.echo('名前変更');
+                    await controller.showReNameModal(
                         context: context,
-                        title: '位置情報取得',
-                        content: Icon(
-                          snapshot.data == true
-                              ? Icons.location_off
-                              : Icons.location_on,
-                          size: 50,
-                        ),
-                        detail: snapshot.data == true
-                            ? '位置情報取得を停止しますか？'
-                            : '位置情報取得を開始しますか？',
-                        buttonAction: () async {
-                          await controller.changeBackgroundLocatorStatus();
+                        size: size,
+                        setState: () {
                           setState(() {});
-                          Navigator.of(
-                            context,
-                            rootNavigator: true,
-                          ).pop();
-                        },
-                        buttonText: '変更',
-                      );
-                    },
-                  );
-                },
-              ),
-              if (kDebugMode)
-                SettingTileCell().common(
-                  '開発用設定',
-                  onTap: () {
-                    Log.echo('開発ボタン');
-                    controller.showDebugModal(context: context, size: size);
+                        });
+                    setState(() {});
                   },
                 ),
-              // const Padding(
-              //   padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              //   child: Align(
-              //     alignment: Alignment.centerLeft,
-              //     child: CustomText(
-              //       text: Strings.SETTING_NOTIFICATION,
-              //       fontSize: 14,
-              //     ),
-              //   ),
-              // ),
-              // SettingTileCell().withSwitch(
-              //   subTitle: 'エリア通知',
-              //   cellAction: (bool) {
-              //     // エリア内に入る or エリアから出るで通知
-              //     controller.areaSwitchValue = bool;
-              //     controller.actionAreaSwitch(value: bool);
-              //     setState(() {});
-              //   },
-              //   switchValue: controller.areaSwitchValue ?? true,
-              // ),
-              // SettingTileCell().withSwitch(
-              //   subTitle: '遅延情報通知',
-              //   cellAction: (bool) {
-              //     // 渋滞情報に変更があったら通知
-              //     controller.delaySwitchValue = bool;
-              //     controller.actionDelaySwitch(value: bool);
-              //     setState(() {});
-              //   },
-              //   switchValue: controller.delaySwitchValue ?? true,
-              // ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: CustomText(
-                    text: Strings.SETTING_OTHER,
-                    fontSize: 14,
-                  ),
-                ),
-              ),
-              SettingTileCell().common(
-                'チュートリアル',
-                onTap: () {
-                  TutorialRoute().push(context);
-                },
-              ),
-              SettingTileCell().common(
-                '使い方',
-                onTap: () {
-                  const HowToUseRoute().push(context);
-                },
-              ),
-              SettingTileCell().common(
-                'お問い合わせ',
-                onTap: () {
-                  // お問い合わせを送信するページ
-                  controller.openInquiry();
-                },
-              ),
-              SettingTileCell().common(
-                'アプリをレビューする',
-                onTap: () {
-                  // ストアページのレビューへ遷移
-                  controller.openReview();
-                },
-              ),
-              SettingTileCell().common(
-                'プライバシーポリシー',
-                onTap: () {
-                  // プライバシーポリシー
-                  controller.openPrivacyPolicy();
-                },
-              ),
-              SettingTileCell().common(
-                'ライセンス',
-                onTap: () {
-                  // ライセンスを表示
-                  const LicenseRoute().push(context);
-                },
-              ),
-              FutureBuilder(
-                  future: controller.getAppVersion(),
+                FutureBuilder(
+                  future: controller.getBackgroundLocatorStatus(),
                   builder: (context, snapshot) {
                     if (snapshot.connectionState != ConnectionState.done) {
                       return const CircularProgressIndicator();
                     }
                     return SettingTileCell().withDetail(
-                        title: 'アプリバージョン', detail: '${snapshot.data}');
-                  }),
-            ],
+                      title: '位置情報取得',
+                      detail: snapshot.data == true ? 'ON' : 'OFF',
+                      onTap: () {
+                        ErrorDialog().showErrorDialog(
+                          context: context,
+                          title: '位置情報取得',
+                          content: Icon(
+                            snapshot.data == true
+                                ? Icons.location_off
+                                : Icons.location_on,
+                            size: 50,
+                          ),
+                          detail: snapshot.data == true
+                              ? '位置情報取得を停止しますか？'
+                              : '位置情報取得を開始しますか？',
+                          buttonAction: () async {
+                            await controller.changeBackgroundLocatorStatus();
+                            setState(() {});
+                            Navigator.of(
+                              context,
+                              rootNavigator: true,
+                            ).pop();
+                          },
+                          buttonText: '変更',
+                        );
+                      },
+                    );
+                  },
+                ),
+                if (kDebugMode)
+                  SettingTileCell().common(
+                    '開発用設定',
+                    onTap: () {
+                      Log.echo('開発ボタン');
+                      controller.showDebugModal(context: context, size: size);
+                    },
+                  ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: CustomText(
+                      text: Strings.SETTING_NOTIFICATION,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                SettingTileCell().common(
+                  '端末の設定を開く',
+                  onTap: () {
+                    // 通知設定画面へ遷移
+                    openAppSettings();
+                  },
+                ),
+                // SettingTileCell().withSwitch(
+                //   subTitle: 'エリア通知',
+                //   cellAction: (bool) {
+                //     // エリア内に入る or エリアから出るで通知
+                //     controller.areaSwitchValue = bool;
+                //     controller.actionAreaSwitch(value: bool);
+                //     setState(() {});
+                //   },
+                //   switchValue: controller.areaSwitchValue ?? true,
+                // ),
+                // SettingTileCell().withSwitch(
+                //   subTitle: '遅延情報通知',
+                //   cellAction: (bool) {
+                //     // 渋滞情報に変更があったら通知
+                //     controller.delaySwitchValue = bool;
+                //     controller.actionDelaySwitch(value: bool);
+                //     setState(() {});
+                //   },
+                //   switchValue: controller.delaySwitchValue ?? true,
+                // ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: CustomText(
+                      text: Strings.SETTING_OTHER,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                SettingTileCell().common(
+                  'チュートリアル',
+                  onTap: () {
+                    TutorialRoute().push(context);
+                  },
+                ),
+                SettingTileCell().common(
+                  '使い方',
+                  onTap: () {
+                    const HowToUseRoute().push(context);
+                  },
+                ),
+                SettingTileCell().common(
+                  'お問い合わせ',
+                  onTap: () {
+                    // お問い合わせを送信するページ
+                    controller.openInquiry();
+                  },
+                ),
+                SettingTileCell().common(
+                  'アプリをレビューする',
+                  onTap: () {
+                    // ストアページのレビューへ遷移
+                    controller.openReview();
+                  },
+                ),
+                SettingTileCell().common(
+                  'プライバシーポリシー',
+                  onTap: () {
+                    // プライバシーポリシー
+                    controller.openPrivacyPolicy();
+                  },
+                ),
+                SettingTileCell().common(
+                  'ライセンス',
+                  onTap: () {
+                    // ライセンスを表示
+                    const LicenseRoute().push(context);
+                  },
+                ),
+                FutureBuilder(
+                    future: controller.getAppVersion(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState != ConnectionState.done) {
+                        return const CircularProgressIndicator();
+                      }
+                      return SettingTileCell().withDetail(
+                          title: 'アプリバージョン', detail: '${snapshot.data}');
+                    }),
+              ],
+            ),
           );
         },
       ),
